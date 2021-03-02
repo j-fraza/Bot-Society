97c163cc095d37b7f231276837335c28d4840e57
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2021-03-02 17:52:42 -0500

    scroll component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -31,9 +32,11 @@ class App extends Component {
         } else {
             return (
                 <div className='tc'>
-                    <h1 className='f1'>Bot Society</h1>
+                    <h1 className='f1'>BotSociety</h1>
                     <SearchBox searchChange = {this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
         
             );
