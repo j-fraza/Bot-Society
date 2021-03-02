677b7aa68c7564b0827c80c27d8849368d9f3880
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2021-03-01 19:34:58 -0500

    search functionality (w/ state)

--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,62 @@
 export const robots = [
   {
     id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
+    name: 'Haydn Kennedy',
+    username: 'Haydn',
     email: 'Sincere@april.biz'
   },
   {
     id: 2,
-    name: 'Ervin Howell',
-    username: 'Antonette',
+    name: 'Suman Mayer',
+    username: 'Suman',
     email: 'Shanna@melissa.tv'
   },
   {
     id: 3,
-    name: 'Clementine Bauch',
-    username: 'Samantha',
+    name: 'Kingston Gilliam',
+    username: 'Kingston',
     email: 'Nathan@yesenia.net'
   },
   {
     id: 4,
-    name: 'Patricia Lebsack',
-    username: 'Karianne',
+    name: 'Alissia Soto',
+    username: 'Alissia',
     email: 'Julianne.OConner@kory.org'
   },
   {
     id: 5,
-    name: 'Chelsey Dietrich',
-    username: 'Kamren',
+    name: 'Akeem Thomas',
+    username: 'Akeem',
     email: 'Lucio_Hettinger@annie.ca'
   },
   {
     id: 6,
-    name: 'Mrs. Dennis Schulist',
-    username: 'Leopoldo_Corkery',
+    name: 'Tierney Tyson',
+    username: 'Tierney_Corkery',
     email: 'Karley_Dach@jasper.info'
   },
   {
     id: 7,
-    name: 'Kurtis Weissnat',
-    username: 'Elwyn.Skiles',
+    name: 'Nick Nunez',
+    username: 'Nunez.Nick',
     email: 'Telly.Hoeger@billy.biz'
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
-    username: 'Maxime_Nienow',
+    name: 'Lennon Combs',
+    username: 'Combs_Lennon',
     email: 'Sherwood@rosamond.me'
   },
   {
     id: 9,
-    name: 'Glenna Reichert',
-    username: 'Delphine',
+    name: 'Lawrence Guthrie',
+    username: 'Lawrence',
     email: 'Chaim_McDermott@dana.io'
   },
   {
     id: 10,
-    name: 'Clementina DuBuque',
-    username: 'Moriah.Stanton',
+    name: 'Ellie-Rose Hansen',
+    username: 'Hansen.Ellie',
     email: 'Rey.Padberg@karina.biz'
   }
 ];
